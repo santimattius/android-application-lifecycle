547b0f6f496574cd0183d42a1d1a7c41f33100b2
Santiago Mattiauda <santimattius@gmail.com>
2024-01-17 21:08:28 -0300

    [improvement] view

--- app/src/main/java/io/github/santimattius/android/application/lifecycle/MainActivity.kt
+++ app/src/main/java/io/github/santimattius/android/application/lifecycle/MainActivity.kt
@@ -3,11 +3,13 @@ package io.github.santimattius.android.application.lifecycle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.santimattius.android.application.lifecycle.ui.theme.AndroidapplicationlifecycleTheme
@@ -22,7 +24,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Greeting("Android")
+                    }
                 }
             }
         }
